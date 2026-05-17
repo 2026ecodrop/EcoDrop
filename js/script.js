2a4196a736d80da57f81795e3ac7395a37272f94
2026ecodrop <2026ecodrop@gmail.com>
2026-05-16 21:53:43 -0300

    atualizando index

---
+++ js/script.js
@@ -0,0 +1,87 @@
+const track = document.getElementById('track');
+const btnNext = document.getElementById('btn-next');
+const btnPrev = document.getElementById('btn-prev');
+const containerIndicadores = document.getElementById('indicadores');
+
+const slides = Array.from(track.children);
+let slideAtual = 0;
+let autoPlay;
+
+slides.forEach((_, index) => {
+    const bolinha = document.createElement('div');
+    bolinha.classList.add('bolinha');
+    if (index === 0) bolinha.classList.add('ativa');
+    bolinha.addEventListener('click', () => moverParaSlide(index));
+    containerIndicadores.appendChild(bolinha);
+});
+
+const bolinhas = document.querySelectorAll('.bolinha');
+
+function atualizarInterface() {
+    track.style.transform = `translateX(-${slideAtual * 100}%)`;
+    bolinhas.forEach((b, i) => {
+        b.classList.toggle('ativa', i === slideAtual);
+    });
+}
+
+function proximoSlide() {
+    slideAtual = (slideAtual + 1) % slides.length;
+    atualizarInterface();
+}
+
+function slideAnterior() {
+    slideAtual = (slideAtual - 1 + slides.length) % slides.length;
+    atualizarInterface();
+}
+
+function moverParaSlide(index) {
+    slideAtual = index;
+    atualizarInterface();
+    reiniciarAutoPlay();
+}
+
+btnNext.addEventListener('click', () => {
+    proximoSlide();
+    reiniciarAutoPlay();
+});
+
+btnPrev.addEventListener('click', () => {
+    slideAnterior();
+    reiniciarAutoPlay();
+});
+
+function iniciarAutoPlay() {
+    autoPlay = setInterval(proximoSlide, 7000);
+}
+
+function reiniciarAutoPlay() {
+    clearInterval(autoPlay);
+    iniciarAutoPlay();
+}
+
+<<<<<<< HEAD
+iniciarAutoPlay();
+
+const btnTopo = document.getElementById('btn-topo');
+const primeiraSecao = document.getElementById('detalhe-maquina');
+
+window.addEventListener('scroll', () => {
+    if (primeiraSecao) {
+        const posicaoSecao = primeiraSecao.getBoundingClientRect().top + window.scrollY;
+        if (window.scrollY >= posicaoSecao) {
+            btnTopo.style.display = 'block';
+        } else {
+            btnTopo.style.display = 'none';
+        }
+    }
+});
+
+btnTopo.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+=======
+iniciarAutoPlay();
+>>>>>>> 713efa0954e10f49b7adfe5f8bccb67e8597a368
